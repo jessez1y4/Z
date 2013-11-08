38e77a18f64d796ae41e41261729ea0aded7a940
Yue Zheng <zhengyuee@gmail.com>
2013-11-08 01:23:20 -0800

    project created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StyleMe::Application.config.secret_key_base = '98761bc5bd54142074bafc52eda86bbd33abd17995cafc4e98ba740bdc07d55c5eed90d275c2fe9b0044bc512783b7efe6abf78e8a2984870c153b829c2f1b1a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StyleMe::Application.config.session_store :cookie_store, key: '_style_me_session'
