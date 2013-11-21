f526b8979ee22187be1320ad71c9d5add32135ba
Yu <yu89126@gmail.com>
2013-11-21 07:59:26 -0600

    now web link works

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,5 +15,15 @@ class PostsController < ApplicationController
   end
 
   def from_web
+    result = Cloudinary::Uploader.upload(params[:web_link])
+    cloudinary_id = "v#{result['version']}/#{result['public_id']}.#{result['format']}"
+    @post = current_user.posts.build(cloudinary_id: cloudinary_id)
+    if @post.save
+      render 'create'
+    else
+      #TODO
+    end
+  rescue CloudinaryException, Errno::ENOENT => e
+    #TODO
   end
 end
