4110dbd22f4613359787dc4d1095867daf542933
Yu <yu89126@gmail.com>
2013-11-28 05:31:13 -0600

    added full name for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131119224729) do
+ActiveRecord::Schema.define(version: 20131128112925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -104,6 +104,7 @@ ActiveRecord::Schema.define(version: 20131119224729) do
     t.datetime "updated_at"
     t.string   "username",                           null: false
     t.string   "description"
+    t.string   "full_name",                          null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
