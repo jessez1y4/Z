ae8e00b0a8453f25a2cf34ac19795499587b22f3
Yu <yu89126@gmail.com>
2013-12-05 06:55:42 -0600

    changed validation messages; added validations for forgot-password-form; refactored data-error

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def conditional_header
     user_signed_in? ? 'partials/navbar' : 'partials/navbar_public'
   end
+
+  def field_err(resource, field)
+    if resource.errors[field].any?
+      "#{resource.errors[field].join(', ')}."
+    else
+      ''
+    end
+  end
 end
