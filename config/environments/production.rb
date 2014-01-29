11c332ac2b6802487b26b323833c533d7e5f7654
Yu <yu89126@gmail.com>
2014-01-29 00:18:00 -0800

    fdsaf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ StyleMe::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( modernizr.js respond.js respond-proxy.html respond.proxy.js fontawesome-webfont.ttf fontawesome-webfont.eot fontawesome-webfont.svg fontawesome-webfont.woff)
+  config.assets.precompile += %w( modernizr.js respond.js respond-proxy.html respond.proxy.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
