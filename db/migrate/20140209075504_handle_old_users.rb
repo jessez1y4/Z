0c72b3e0ff2596bb67373d66a599da1a73a1af23
Yu <yu89126@gmail.com>
2014-02-09 22:41:15 -0800

    fixed a bug where chinese characters generate nil slug

--- db/migrate/20140209075504_handle_old_users.rb
+++ db/migrate/20140209075504_handle_old_users.rb
@@ -1,5 +1,7 @@
 class HandleOldUsers < ActiveRecord::Migration
   def change
+    add_column :users, :changed_username, :boolean, default: false, null: false
+
     User.find_each(&:save!)
   end
 end
