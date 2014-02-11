b08575ef466e81a7c81dd6904e70f5a980d4cf7b
Yu <yu89126@gmail.com>
2014-02-11 14:08:02 -0800

    fdsaf

--- db/migrate/20140209075504_handle_old_users.rb
+++ db/migrate/20140209075504_handle_old_users.rb
@@ -3,5 +3,10 @@ class HandleOldUsers < ActiveRecord::Migration
     add_column :users, :changed_username, :boolean, default: false, null: false
 
     User.find_each(&:save!)
+
+    User.find_each do |u|
+      u.changed_username = false
+      u.save!
+    end
   end
 end
